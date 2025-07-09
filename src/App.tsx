e1589dc7cf933637f96df4394f9af6dc684e9b04
Pritam <pritamsamaddar840@gmail.com>
2025-07-09 23:42:15 +0530

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,9 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 export default function App() {
-  return ( 
-     <Provider store={store}>
-          <AppRoutes />  
-    </Provider> 
+  return (
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   );
 }
- {/* <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/new" element={<NewProject />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/project/:id" element={<ProjectDetail />} />
-        <Route path="/search" element={<Search />} />
-      </Routes> */}
